4681430a041a57ce21f5fff768b4a47b7928fe50
Sean Firsching <seanfirsching@gmail.com>
2023-01-31 18:52:42 -0500

    Added csv parsers

--- Data Scripts/readFile.py
+++ Data Scripts/readFile.py
@@ -2,7 +2,6 @@ import json
 import csv
 import os
 import re
-import pandas as pd
 
 
 def natural_sort(l):
@@ -13,8 +12,86 @@ def natural_sort(l):
     return sorted(l, key=alphanum_key)
 
 
-folder_path = 'Data/movies/cast'
-files = natural_sort(os.listdir(folder_path))
-for count, file in enumerate(files):
-    jsonObj = pd.read_json(path_or_buf=f"{folder_path}/{file}", lines=True)
-    print(jsonObj.to_string())
+def get_movie_cast_csv():
+    folder_path = '../Data/movies/cast'
+    csvWrite = csv.writer(open("../Data/csv/cast.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(["movie_id", "person_id", "character", "credit_id"])
+    files = natural_sort(os.listdir(folder_path))
+    for file in files:
+        for line in open(f"{folder_path}/{file}", "r").readlines():
+            jsonObj = json.loads(line)
+            for person in jsonObj['cast']:
+                csvWrite.writerow(
+                    [jsonObj['id'], person['id'], person['character'], person['credit_id']])
+
+
+def get_movie_crew_csv():
+    folder_path = '../Data/movies/cast'
+    csvWrite = csv.writer(open("../Data/csv/crew.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["movie_id", "person_id", "department", "job", "credit_id"])
+    files = natural_sort(os.listdir(folder_path))
+    for file in files:
+        for line in open(f"{folder_path}/{file}", "r").readlines():
+            jsonObj = json.loads(line)
+            for person in jsonObj['crew']:
+                csvWrite.writerow(
+                    [jsonObj['id'], person['id'], person['department'], person['job'], person['credit_id']])
+
+
+def get_movies_csv():
+    folder_path = '../Data/movies'
+    csvWrite = csv.writer(open("../Data/csv/movies.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["movie_id", "imdb_id", "adult", "title", "genres", "production_companies", "release_date", "budget", "revenue", "status", "spoken_languages", "runtime"])
+    files = natural_sort([f for f in os.listdir(
+        folder_path) if os.path.isfile(folder_path+'/'+f)])
+    for file in files:
+        for line in open(f"{folder_path}/{file}", "r").readlines():
+            jsonObj = json.loads(line)
+            csvWrite.writerow(
+                [jsonObj['id'], jsonObj['imdb_id'], jsonObj['adult'], jsonObj['title'], [genre['id'] for genre in jsonObj['genres']], [company['id'] for company in jsonObj['production_companies']],
+                 jsonObj['release_date'], jsonObj['budget'], jsonObj['revenue'], jsonObj['status'], [
+                     languages['iso_639_1'] for languages in jsonObj['spoken_languages']],
+                 jsonObj['runtime']])
+
+
+def get_tv_shows_csv():
+    folder_path = '../Data/people'
+    csvWrite = csv.writer(open("../Data/csv/people.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["tv_show_id", "imdb_id", "adult", "title", "genres", "production_companies", "release_date", "budget", "revenue", "status", "spoken_languages", "runtime"])
+    files = natural_sort([f for f in os.listdir(
+        folder_path) if os.path.isfile(folder_path+'/'+f)])
+    for file in files:
+        for line in open(f"{folder_path}/{file}", "r").readlines():
+            jsonObj = json.loads(line)
+            csvWrite.writerow(
+                [jsonObj['id'], jsonObj['imdb_id'], jsonObj['adult'], jsonObj['title'], [genre['id'] for genre in jsonObj['genres']], [company['id'] for company in jsonObj['production_companies']],
+                 jsonObj['release_date'], jsonObj['budget'], jsonObj['revenue'], jsonObj['status'], [
+                     languages['iso_639_1'] for languages in jsonObj['spoken_languages']],
+                 jsonObj['runtime']])
+            exit()
+
+
+def get_people_csv():
+    folder_path = '../Data/people'
+    csvWrite = csv.writer(open("../Data/csv/people.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["person_id", "imdb_id", "adult", "popularity", "name", "birthday", "deathday", "gender", "place_of_birth"])
+    files = natural_sort([f for f in os.listdir(
+        folder_path) if os.path.isfile(folder_path+'/'+f)])
+    for file in files:
+        for line in open(f"{folder_path}/{file}", "r").readlines():
+            jsonObj = json.loads(line)
+            csvWrite.writerow(
+                [jsonObj['id'], jsonObj['imdb_id'], jsonObj['adult'], jsonObj['popularity'],
+                 jsonObj['name'], jsonObj['birthday'], jsonObj['deathday'], jsonObj['gender'], jsonObj['place_of_birth']])
+
+
+get_people_csv()
---
+++ Data/csv/test.py
@@ -0,0 +1,3 @@
+arr = [{'id': 18, 'name': 'Drama'}, {
+    'id': 35, 'name': 'Comedy'}, {'id': 80, 'name': 'Crime'}]
+print([g['id'] for g in arr])
