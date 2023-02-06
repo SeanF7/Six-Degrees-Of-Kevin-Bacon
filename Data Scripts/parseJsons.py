c8f3ba5767174ecb0846b51bc6ac9f0b7646cc90
Sean Firsching <seanfirsching@gmail.com>
2023-02-06 13:45:36 -0500

    Added JSON parser for tv data

--- Data Scripts/parseJsons.py
+++ Data Scripts/parseJsons.py
@@ -60,22 +60,49 @@ def get_movies_csv():
 
 
 def get_tv_shows_csv():
-    folder_path = '../Data/tv-shows/shows'
     csvWrite = csv.writer(open("../Data/csv/tv_episodes.csv", "w",
                                newline='', encoding='utf-8'))
     csvWrite.writerow(
-        ["tv_show_id", "imdb_id", "adult", "title", "genres", "production_companies", "release_date", "budget", "revenue", "status", "spoken_languages", "runtime"])
-    files = natural_sort([f for f in os.listdir(
-        folder_path) if os.path.isfile(folder_path+'/'+f)])
-    for file in files:
-        for line in open(f"{folder_path}/{file}", "r").readlines():
-            jsonObj = json.loads(line)
-            csvWrite.writerow(
-                [jsonObj['id'], jsonObj['imdb_id'], jsonObj['adult'], jsonObj['title'], [genre['id'] for genre in jsonObj['genres']], [company['id'] for company in jsonObj['production_companies']],
-                 jsonObj['release_date'], jsonObj['budget'], jsonObj['revenue'], jsonObj['status'], [
-                     languages['iso_639_1'] for languages in jsonObj['spoken_languages']],
-                 jsonObj['runtime']])
-            exit()
+        ["tv_id", "season", "episode", "air_date", "name", "runtime", "vote_average", "vote_count"])
+    for line in open("../Data/tv-shows/shows/shows.jsonl", "r").readlines():
+        jsonObj = json.loads(line)
+        csvWrite.writerow([jsonObj["tv_id"], jsonObj["season"],
+                           jsonObj["episode"], jsonObj["air_date"],
+                           jsonObj["name"], jsonObj["runtime"],
+                           jsonObj["vote_average"], jsonObj["vote_count"]])
+
+
+def get_tv_show_cast_csv():
+
+    csvWrite = csv.writer(open("../Data/csv/tv_credits.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["tv_id", "season", "episode", "name", "person_id", "gender", "adult", "popularity", "credit_id"])
+    for line in open("../Data/tv-shows/credits/credits.jsonl", "r").readlines():
+        jsonObj = json.loads(line)
+        for cast_member in jsonObj["cast"]:
+            csvWrite.writerow([jsonObj["tv_id"], jsonObj["season"],
+                               jsonObj["episode"], cast_member["name"],
+                               cast_member["id"], cast_member["gender"],
+                               cast_member["adult"], cast_member["popularity"],
+                               cast_member["credit_id"]])
+
+
+def get_tv_show_crew_csv():
+
+    csvWrite = csv.writer(open("../Data/csv/tv_crew.csv", "w",
+                               newline='', encoding='utf-8'))
+    csvWrite.writerow(
+        ["tv_id", "season", "episode", "name", "person_id", "gender", "job", "department", "credit_id", "adult", "popularity"])
+    for line in open("../Data/tv-shows/credits/credits.jsonl", "r").readlines():
+        jsonObj = json.loads(line)
+        for crew_member in jsonObj["crew"]:
+            csvWrite.writerow([jsonObj["tv_id"], jsonObj["season"],
+                               jsonObj["episode"], crew_member["name"],
+                               crew_member["id"], crew_member["gender"],
+                               crew_member["job"], crew_member["department"],
+                               crew_member["credit_id"], crew_member["adult"],
+                               crew_member["adult"], crew_member["popularity"]])
 
 
 def get_people_csv():
@@ -94,7 +121,4 @@ def get_people_csv():
                  jsonObj['name'], jsonObj['birthday'], jsonObj['deathday'], jsonObj['gender'], jsonObj['place_of_birth']])
 
 
-get_people_csv()
-get_movies_csv()
-get_movie_cast_csv()
-get_movie_crew_csv()
+get_tv_show_crew_csv()
