56a9103dee67156d597fc105e22b6e3c4f606786
Sean Firsching <seanfirsching@gmail.com>
2023-05-04 14:56:35 -0400

    Removed data related files that are no longer required.

--- Data/Data Scripts/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import json as js
-
-testdf = pd.read_csv("tv_shows_test.csv")
-seasdf = pd.DataFrame(testdf['seasons'])
-print(seasdf.to_json(orient='records', convert_quotes=True))
